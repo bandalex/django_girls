f4d96baf21c6476bcf35ea24d25fb3b67aa82834
bandalex <bandalex@mail.ru>
2022-09-10 22:17:36 +0300

    Delete post

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
